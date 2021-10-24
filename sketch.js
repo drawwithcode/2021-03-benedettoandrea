d4a57529d963599e7002a94d3cd6b3e63f6aeccf
Andrea Benedetto <34688096+benedettoandrea@users.noreply.github.com>
2021-10-24 21:00:57 +0200

    further development and cleanup

--- sketch.js
+++ sketch.js
@@ -1,22 +1,43 @@
-// audiovisual experience based on Autechre's elseq 1–5, AE_LIVE and NTS Sessions 1–4 visual language, originally designed by tDR.
-// audio: Autechre - 2000-XX-XX Germany-Berlin - 16 (https://archive.org/details/Autechre2003-03-30)
+// audiovisual experience based on Autechre's elseq 1–5, AE_LIVE and NTS Sessions 1–4 visual language, originally designed by tDR (https://www.thedesignersrepublic.com/ae-vs-tdr).
+/* tracks:
+   1) Autechre Live at Flex on 1996-02-15 (https://archive.org/details/Autechre1996-02-15)
+   2) Autechre - 1999-07-05 Peel Session - 2 Blifil (https://archive.org/details/Autechre1999-07-06)
+   3) Autechre - 2000-XX-XX Germany-Berlin - 16 (https://archive.org/details/Autechre2000-00-00.Autechre2000-00-00)
+   4) Autechre Live at BBC Radio 3 on 2003-03-30 (https://archive.org/details/Autechre2003-03-30)
+*/
 
-// theme selector
-var backgroundToggle = 255;
+// theme selector (255 = light, 0 = dark)
+var currentTheme = 255;
+var flashingImages = 1;
 
-var subd1 = 12;
-var subd2 = 24;
-var subd3 = 36;
+// subdivision values for the grid system
+const subd1 = 12;
+const subd2 = 24;
+// const subd3 = 36; (unused)
 
+// audio
 var soundFile, amplitude, fft;
 
-var octaves = 8;
-var falloff = 0.5;
+// noise
+const octaves = 8;
+const falloff = 0.5;
 
-var alphaEff = 1;
+// assets
+var imageFile = new Array();
+var imageList;
+var imageShow;
+var trackList = new Array();
+
+// audio control
+var trackState = "pause";
+var trackCurrentlyPlaying = 0;
+var trackNo = 0;
+
+// var alphaEff = 1; (unused)
 
 function preload() {
-  imageFile = loadImage("./assets/images/AE_LIVE.jpg");
+  imageList = loadJSON("images.json");
+  trackList = loadJSON("tracks.json");
 }
 
 function setup() {
@@ -27,63 +48,80 @@ function setup() {
   fill(10);
   noStroke();
 
-  fft = new p5.FFT();
-  peakDetect = new p5.PeakDetect("treble");
-  soundFile = document.getElementById("soundFile");
+  // load images inside an array
+  for (let i = 0; i < imageList.images.length; i++) {
+    imageFile[i] = loadImage("./assets/images/" + imageList.images[i]);
+  }
 
+  console.log("track no. " + trackNo + " (" + trackList.tracks[trackNo] + ")");
+
+  // select a random image insie the array to show before starting
+  randomImage();
+
+  // audio setup
+  fft = new p5.FFT();
+  peakDetect = new p5.PeakDetect(5200, 14000, 0.5);
+  soundFile = document.getElementById("soundFile");
   let context = getAudioContext();
   // wire all media elements up to the p5.sound AudioContext
   for (let elem of selectAll("audio").concat(selectAll("video"))) {
     let mediaSource = context.createMediaElementSource(elem.elt);
     mediaSource.connect(p5.soundOut);
   }
+
+  // noise setup
+  noiseDetail(octaves, falloff);
 }
 
 function draw() {
-  background(backgroundToggle);
+  background(currentTheme);
 
-  var bass = fft.getEnergy(20, 400);
-  var mid = fft.getEnergy(400, 2600);
-  var high = fft.getEnergy(2600, 5200);
-  // var treble = fft.getEnergy(5200, 1400);
-
+  // audio analysis
   fft.analyze();
   peakDetect.update(fft);
 
+  // frequencies to analyse
+  var bass = fft.getEnergy(20, 400);
+  var mid = fft.getEnergy(400, 2600);
+  // var high = fft.getEnergy(2600, 5200);
+  // var treble = fft.getEnergy(5200, 14000);
+
   var i1 = 0;
   var i2 = 0;
-  // var i3 = 0;
-
-  noiseDetail(octaves, falloff);
+  // var i3 = 0; (unused)
 
   // image manipulation
-  push();
-  if (mid > 150 && mid < 200) {
-    // raster effect: disabled because of extreme lag if the device isn't pretty powerful.
-    if (backgroundToggle === 255) {
-      //filter(INVERT);
-      //blendMode(DIFFERENCE);
-    } else if (backgroundToggle === 0) {
-      //blendMode(DIFFERENCE);
+  if (flashingImages == 1) {
+    push();
+    if (mid > 127 && mid < 255) {
+      // raster effect: disabled most of it because it can cause extreme lag if a device isn't pretty powerful.
+      if (currentTheme === 255) {
+        //filter(INVERT);
+        blendMode(DIFFERENCE);
+      } else if (currentTheme === 0) {
+        //blendMode(DIFFERENCE);
+      }
+      image(imageFile[imageShow], 0, 0, windowWidth, windowHeight);
     }
-    image(imageFile, 0, 0, windowWidth, windowHeight);
+    if (peakDetect.isDetected) {
+      randomImage();
+    }
+    pop();
   }
-  pop();
-
   // subdivision #1
   var varDim1 = map(
     bass,
     0,
     255,
-    (width / subd1) * 0.05,
-    width / subd1 - windowWidth / 1000
+    (width / subd1) * 0.005,
+    (width / subd1) * 1.25
   );
   if (width < height) {
     for (var x1 = 0 + width / subd1 / 2; x1 < width; x1 += width / subd1) {
       i1 = 0;
       for (
         var y1 = 0 + width / subd1 / 2;
-        y1 < windowHeight - (width / subd1) * 4;
+        y1 < windowHeight - (width / subd1) * 4; // vertical screens
         y1 += width / subd1
       ) {
         i1++;
@@ -92,8 +130,8 @@ function draw() {
           frameCount / 2500 + x1 / 250,
           frameCount / 2500 + y1 / 250
         );
-        if (noiseColor1 * 10 > 2.5 && noiseColor1 * 10 < 5) {
-          if (noiseColor1 * 10 > 3.75 && noiseColor1 * 10 < 5) {
+        if (noiseColor1 * 10 > 2.625 && noiseColor1 * 10 < 5.625) {
+          if (noiseColor1 * 10 > 5 && noiseColor1 * 10 < 5.625) {
             rect(x1, y1, varDim1, varDim1);
           } else {
             ellipse(x1, y1, varDim1);
@@ -108,7 +146,7 @@ function draw() {
       i1 = 0;
       for (
         var y1 = 0 + width / subd1 / 2;
-        y1 < windowHeight - (width / subd1) * 2;
+        y1 < windowHeight - (width / subd1) * 2; // horizontal screens
         y1 += width / subd1
       ) {
         i1++;
@@ -117,8 +155,8 @@ function draw() {
           frameCount / 2500 + x1 / 250,
           frameCount / 2500 + y1 / 250
         );
-        if (noiseColor1 * 10 > 2.5 && noiseColor1 * 10 < 5) {
-          if (noiseColor1 * 10 > 3.75 && noiseColor1 * 10 < 5) {
+        if (noiseColor1 * 10 > 2.625 && noiseColor1 * 10 < 5.625) {
+          if (noiseColor1 * 10 > 5 && noiseColor1 * 10 < 5.625) {
             rect(x1, y1, varDim1, varDim1);
           } else {
             ellipse(x1, y1, varDim1);
@@ -135,7 +173,7 @@ function draw() {
     mid,
     0,
     255,
-    (width / subd2) * 0.05,
+    (width / subd2) * 0.005,
     width / subd2 - windowWidth / 1000
   );
   for (var x2 = 0 + width / subd2 / 2; x2 < width; x2 += width / subd2) {
@@ -147,8 +185,8 @@ function draw() {
         frameCount / 2500 + x2 / 250,
         frameCount / 2500 + y2 / 250
       );
-      if (noiseColor2 * 10 > 5 && noiseColor2 * 10 < 7.5) {
-        if (noiseColor2 * 10 > 6.75 && noiseColor2 * 10 < 7.5) {
+      if (noiseColor2 * 10 > 5 && noiseColor2 * 10 < 6.9375) {
+        if (noiseColor2 * 10 > 6.3125 && noiseColor2 * 10 < 6.9375) {
           rect(x2, y2, varDim2);
         } else {
           ellipse(x2, y2, varDim2);
@@ -159,7 +197,7 @@ function draw() {
     }
   }
 
-  // subdivision #3
+  // subdivision #3 (unused)
   /*
   var varDim3 = map(
     high,
@@ -194,16 +232,21 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function randomImage() {
+  imageShow = getRandomInt(0, imageFile.length - 1);
+  console.log("image no. " + getRandomInt(0, imageFile.length - 1));
+}
+
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
 function toggleP5Theme() {
-  if (backgroundToggle === 255) {
-    backgroundToggle = 0;
+  if (currentTheme === 255) {
+    currentTheme = 0;
     fill(255);
-  } else if (backgroundToggle === 0) {
-    backgroundToggle = 255;
+  } else if (currentTheme === 0) {
+    currentTheme = 255;
     fill(10);
   }
 }
@@ -212,9 +255,11 @@ function togglePlay() {
   if (soundFile.paused) {
     soundFile.play();
     loop();
+    trackState = "play";
   } else {
     soundFile.pause();
     noLoop();
+    trackState = "pause";
   }
 }
 
@@ -222,8 +267,34 @@ function saveScreenshot() {
   saveCanvas("myCanvas", "png");
 }
 
+function toggleImages() {
+  if (flashingImages === 1) {
+    flashingImages = 0;
+  } else if (flashingImages === 0) {
+    flashingImages = 1;
+  }
+}
+
+function toggleAudio() {
+  var track = document.getElementById("track");
+  trackNo++;
+  if (trackNo == trackList.tracks.length) {
+    trackNo = 0;
+  }
+  console.log("track no. " + trackNo + " (" + trackList.tracks[trackNo] + ")");
+  track.src = "./assets/tracks/" + trackList.tracks[trackNo];
+  document.getElementById("soundFile").load();
+  if (trackState == "play") {
+    soundFile.play();
+    loop();
+  } else {
+    soundFile.pause();
+    noLoop();
+  }
+}
+
 function touchStarted() {
-  if (getAudioContext().state !== "running") {
+  if (getAudioContext().trackState !== "running") {
     getAudioContext().resume();
   }
 }
